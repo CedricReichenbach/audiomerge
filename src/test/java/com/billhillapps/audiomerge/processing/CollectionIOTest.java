89d7b8b29e56256a4a3add58661f365b58d10db4
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2016-12-28 19:29:12 +0100

    Add unit tests for test collections b and c

--- src/test/java/com/billhillapps/audiomerge/processing/CollectionIOTest.java
+++ src/test/java/com/billhillapps/audiomerge/processing/CollectionIOTest.java
@@ -3,6 +3,7 @@ package com.billhillapps.audiomerge.processing;
 import static com.billhillapps.audiomerge.test.ArtistMatcher.isArtist;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.StringEndsWith.endsWith;
 import static org.junit.Assert.assertThat;
 
@@ -38,6 +39,24 @@ public class CollectionIOTest {
 		assertThat(collectionA.getArtists(), hasItem(isArtist("Hussalonia")));
 		assertThat(collectionA.getArtists(), hasItem(isArtist("Alessandro Moreschi")));
 		assertThat(collectionA.getArtists(), hasItem(isArtist("Kevin MacLeod")));
+
+		MusicCollection collectionB = CollectionIO.fromDirectory(collectionPathB);
+
+		assertThat(collectionB.getTitle(), endsWith("collection-b"));
+		assertThat(collectionB.getArtists().size(), is(2));
+		assertThat(collectionB.getArtists(), hasItem(isArtist("Ludwig van Beethoven")));
+		assertThat(collectionB.getArtists(), hasItem(isArtist("Kevin MacLeod")));
+		assertThat(collectionB.getArtists(), not(hasItem(isArtist("Hussalonia"))));
+		assertThat(collectionB.getArtists(), not(hasItem(isArtist("Alessandro Moreschi"))));
+
+		MusicCollection collectionC = CollectionIO.fromDirectory(collectionPathC);
+
+		assertThat(collectionC.getTitle(), endsWith("collection-c"));
+		assertThat(collectionC.getArtists().size(), is(1));
+		assertThat(collectionC.getArtists(), hasItem(isArtist("Hussalonia")));
+		assertThat(collectionC.getArtists(), not(hasItem(isArtist("Ludwig van Beethoven"))));
+		assertThat(collectionC.getArtists(), not(hasItem(isArtist("Kevin MacLeod"))));
+		assertThat(collectionC.getArtists(), not(hasItem(isArtist("Alessandro Moreschi"))));
 
 		// TODO: Other collections
 	}
