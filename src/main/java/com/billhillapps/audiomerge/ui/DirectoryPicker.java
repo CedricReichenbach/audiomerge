e4ee0122322ea6cd72801d11bd91e797e5012acb
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2016-12-30 23:28:25 +0100

    Add text field to directory picker

--- src/main/java/com/billhillapps/audiomerge/ui/DirectoryPicker.java
+++ src/main/java/com/billhillapps/audiomerge/ui/DirectoryPicker.java
@@ -1,6 +1,11 @@
 package com.billhillapps.audiomerge.ui;
 
+import java.io.File;
+import java.nio.file.Path;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
@@ -9,17 +14,30 @@ public class DirectoryPicker extends Pane {
 
 	private final DirectoryChooser directoryChooser;
 
+	private Path chosenPath;
+
 	public DirectoryPicker(Stage primaryStage) {
 		super();
+
+		HBox container = new HBox();
+		this.getChildren().add(container);
 
 		directoryChooser = new DirectoryChooser();
 		directoryChooser.setTitle("Select root directory of music collection");
 
+		TextField pathField = new TextField();
+
 		Button button = new Button("Select...");
 		button.setOnAction(event -> {
-			directoryChooser.showDialog(primaryStage);
+			File chosenFile = directoryChooser.showDialog(primaryStage);
+			if (chosenFile == null)
+				return;
+
+			chosenPath = chosenFile.toPath();
+			pathField.setText(chosenPath.toString());
 		});
 
-		this.getChildren().add(button);
+		container.getChildren().add(pathField);
+		container.getChildren().add(button);
 	}
 }
