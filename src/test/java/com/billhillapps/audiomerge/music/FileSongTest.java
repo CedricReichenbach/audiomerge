e1860a4e721207e407c4080638803f26876daa1d
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2017-03-25 16:34:56 +0100

    Reorganize test song loading in FileSongTest

--- src/test/java/com/billhillapps/audiomerge/music/FileSongTest.java
+++ src/test/java/com/billhillapps/audiomerge/music/FileSongTest.java
@@ -20,18 +20,13 @@ public class FileSongTest {
 	public TemporaryFolder destinationFolder = new TemporaryFolder();
 	private Path destinationPath;
 
-	private Path id3v1File;
-	private Path noTagFile;
-	private Path writeProtectedFile;
+	private Path cornerCasesDir;
 
 	@Before
 	public void setUp() throws Exception {
 		destinationPath = destinationFolder.getRoot().toPath();
 
-		Path cornerCasesDir = Paths.get(ClassLoader.getSystemResource("corner-cases").toURI());
-		id3v1File = cornerCasesDir.resolve("id3v1.mp3");
-		noTagFile = cornerCasesDir.resolve("no-tag.mp3");
-		writeProtectedFile = cornerCasesDir.resolve("write-protected.mp3");
+		cornerCasesDir = Paths.get(ClassLoader.getSystemResource("corner-cases").toURI());
 	}
 
 	@After
@@ -40,14 +35,14 @@ public class FileSongTest {
 
 	@Test
 	public void canLoadFilesWithNoTag() throws Exception {
-		Song noTagSong = new FileSong(noTagFile);
+		Song noTagSong = new FileSong(cornerCasesDir.resolve("no-tag.mp3"));
 
 		assertThat(noTagSong.getArtistName(), isEmptyOrNullString());
 	}
 
 	@Test
 	public void canOverrideArtistOfID3v1() throws Exception {
-		final Song id3v1Song = new FileSong(id3v1File);
+		final Song id3v1Song = new FileSong(cornerCasesDir.resolve("id3v1.mp3"));
 
 		id3v1Song.setArtistName("foobar");
 
@@ -61,6 +56,7 @@ public class FileSongTest {
 
 	@Test
 	public void copyOfWriteProtectedCanBeOverridden() throws Exception {
+		final Path writeProtectedFile = cornerCasesDir.resolve("write-protected.mp3");
 		writeProtectedFile.toFile().setReadOnly();
 
 		final Song writeProtectedSong = new FileSong(writeProtectedFile);
